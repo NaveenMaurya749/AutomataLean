c567288beb09db510981cd54b049ff42bd5032b5
NaveenMaurya749 <naveenmaurya749@gmail.com>
2025-04-18 02:38:42 +0530

    furter definitions: instant desc,  evaluation

--- AutomataLean/PDAs.lean
+++ AutomataLean/PDAs.lean
@@ -17,8 +17,41 @@ Mathematicaly, a DFA is a 5-tuple (Q, Σ, δ, q0, F) where
 
 This has already been implemented in Mathlib.Computability.DFA
 -/
+
+/-
+structure DFA.{u, v} (α : Type u) (σ : Type v) : Type (max u v)
+number of parameters: 2
+fields:
+  DFA.step : σ → α → σ
+  DFA.start : σ
+  DFA.accept : Set σ
+constructor:
+  DFA.mk.{u, v} {α : Type u} {σ : Type v} (step : σ → α → σ) (start : σ) (accept : Set σ) : DFA α σ
+-/
 #print DFA
+
+/-
+structure NFA.{u, v} (α : Type u) (σ : Type v) : Type (max u v)
+number of parameters: 2
+fields:
+  NFA.step : σ → α → Set σ
+  NFA.start : Set σ
+  NFA.accept : Set σ
+constructor:
+  NFA.mk.{u, v} {α : Type u} {σ : Type v} (step : σ → α → Set σ) (start accept : Set σ) : NFA α σ
+-/
 #print NFA
+
+/-
+structure εNFA.{u, v} (α : Type u) (σ : Type v) : Type (max u v)
+number of parameters: 2
+fields:
+  εNFA.step : σ → Option α → Set σ
+  εNFA.start : Set σ
+  εNFA.accept : Set σ
+constructor:
+  εNFA.mk.{u, v} {α : Type u} {σ : Type v} (step : σ → Option α → Set σ) (start accept : Set σ) : εNFA α σ
+-/
 #print εNFA
 
 /-
@@ -28,14 +61,14 @@ Mathematicaly, a PDA is a 6-tuple (Q, Σ, Γ, δ, q0, F) where
 -- Q is a finite set of states
 -- Σ is a finite set of input symbols,
 -- Γ is a finite set of stack symbols,
--- δ : Q × Σ × Γ → Q × Γ* is the transition function,
+-- δ : Q × Σ × Γ → Set (Q × Γ*) is the transition function,
 -- q0 ∈ Q is the initial state,
 -- and F ⊆ Q is the set of accepting states.
 -/
 
-structure PDA (Q : Type u) (σ : Type v) (Γ : Type w) where
+structure PDA (Q : Type u) (α : Type v) (Γ : Type w) where
   /-- A transition function from state to state labelled by the alphabet. -/
-  transition : Q → Option σ → Γ → Q × (Language Γ)
+  transition : Q → Option α → Γ → Q × (List Γ)
   inital_state : Q
   initial_stack : Γ
   accept : Q → Prop
@@ -44,11 +77,16 @@ structure PDA (Q : Type u) (σ : Type v) (Γ : Type w) where
 
 namespace PDA
 
-variable {Q : Type u} {σ : Type v} {Γ : Type w} (M: PDA Q σ Γ)
+variable {Q : Type u} {α : Type v} {Γ : Type w} (M : PDA Q α Γ)
 
 -- Inhabited instance for PDA
-instance [Inhabited Q] [Inhabited Γ]: Inhabited (PDA Q σ Γ) :=
-    ⟨PDA.mk (fun _ _ _ => (default, default)) (default) (default) (fun _ => default)⟩
+instance [Inhabited Q] [Inhabited Γ]: Inhabited (PDA Q α Γ) :=
+    ⟨PDA.mk (fun _ _ _ => default) (default) (default) (fun _ => default)⟩
+
+structure InstantDesc (M : PDA Q α Γ) where
+  state : Q
+  input_tape : List α
+  stack_tape : List Γ
 
 /-Example of DFA-/
 def my_dfa : DFA Char Bool :=
@@ -58,7 +96,9 @@ def my_dfa : DFA Char Bool :=
 
 #eval my_dfa.eval "sdas".toList
 
-/-- Example of a PDA that accepts the language of balanced parentheses. -/
+-- Example of a PDA that accepts the language of balanced parentheses.
+
+--namespace Balanced
 
 inductive Parentheses : Type
 | left | right
@@ -66,7 +106,7 @@ deriving Inhabited, Repr
 
 inductive States : Type
 | q0 | q1 | q2
-deriving Inhabited, Repr
+deriving Inhabited, Repr, Fintype
 
 inductive Stack : Type
 | A | Z
@@ -77,13 +117,13 @@ open Parentheses States Stack
 def my_pda : PDA States Parentheses Stack :=
   ⟨(fun q x y =>
     match q, x, y with
-    | q0, some left,  Z => (q0, {[A, Z]})
-    | q0, some left,  A => (q0, {[A, A]})
-    | q1, none,       Z => (q1, {[Z]})
-    | q1, none,       A => (q1, {[A]})
-    | q1, some right, A => (q1, {[]})
-    | q2, none,       Z => (q2, {[Z]})
-    | _, _, _ => (q2, {[]})
+    | q0, some left,  Z => (q0, [A, Z])
+    | q0, some left,  A => (q0, [A, A])
+    | q1, none,       Z => (q1, [Z])
+    | q1, none,       A => (q1, [A])
+    | q1, some right, A => (q1, [])
+    | q2, none,       Z => (q2, [Z])
+    | _, _, _           => (q2, [])
     ),
     q0,
     Z,
@@ -91,17 +131,69 @@ def my_pda : PDA States Parentheses Stack :=
   ⟩
 #note "Yayyyyyyyyyyyy!!!!!!!!!!!!!"
 
-/-
-def PDA.evalFrom {Q : Type u} {σ : Type v} {Γ : Type w} [Finite Γ] (pda : PDA Q σ Γ) (q : Q)
-  : (input : List σ) → List Γ → Bool :=
-  fun M start stack =>
-    match input with
-    | [] => pda.accept q
-    | x :: xs =>
-      let (q', stack') := pda.transition q (some x) (List.headD stack)
-      PDA.evalFrom pda xs (stack' ++ stack.tail) q'
+-- end Balanced
 
-#print PDA
+#check PDA.transition
 
-end PDA
+def evalStep (M : PDA Q α Γ) (desc : InstantDesc M) : InstantDesc M :=
+  have ⟨q, input, stack⟩ := desc
+  match input, stack with
+  | _, [] => ⟨q, input, stack⟩
+  | [], top::rest =>
+    let (q', new_stack) := M.transition q none top
+    ⟨q', [], new_stack ++ rest⟩
+  | x::xs, top::rest =>
+    let (q', new_stack) := M.transition q (some x) top
+    ⟨q', xs, new_stack ++ rest⟩
+
+/-[@simp]
+theorem evalStep_blank (M : PDA Q α Γ) (desc : InstantDesc M) : M.evalStep desc = desc := by
+  simp [evalStep]
+  split
+  · sim
+  · simp
+  · sorry
+
 -/
+
+lemma evalStep_decrements_input (desc : InstantDesc M) (hi : desc.input_tape ≠ []) (hs : desc.stack_tape ≠ []) :
+  (M.evalStep desc).input_tape.length < desc.input_tape.length := by
+    match desc with
+    | ⟨q, _, []⟩ => contradiction
+    | ⟨_, _, top::rest⟩ =>
+      simp only [List.length_cons]
+      unfold evalStep
+      simp
+      split
+      · simp
+      · simp
+
+#do_later "Add [@simp] lemmas"
+
+def evalFrom (M : PDA Q α Γ) (desc : InstantDesc M) : Q :=
+  match desc with
+  | ⟨q, _, []⟩ => q
+  | ⟨_, _, _⟩ =>
+    evalFrom M (evalStep M desc)
+termination_by desc.input_tape.length
+decreasing_by
+  sorry
+  -- simp [evalStep]
+  -- split
+  -- todo "Prove this terminates"
+  -- -- sorry
+
+def eval (M : PDA Q α Γ) (input : List α) : Q :=
+  M.evalFrom ⟨M.inital_state, input, [M.initial_stack]⟩
+
+def accepts (M : PDA Q α Γ) (input : List α) : Prop :=
+  M.accept (M.eval input)
+
+/-
+failed to synthesize
+  Decidable (my_pda.accepts [left, right, left, right])
+
+Additional diagnostic information may be available using the `set_option diagnostics true` command.
+-/
+set_option diagnostics true
+#eval my_pda.accepts [left, right, left, right]
