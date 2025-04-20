b60e3463b018a8d2ed542d3455dc00c5b9425da6
NaveenMaurya749 <naveenmaurya749@gmail.com>
2025-04-21 03:46:55 +0530

    imported all modules (so far) to root library

--- AutomataLean.lean
+++ AutomataLean.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `AutomataLean` library.
 -- Import modules here that should be built as part of the library.
 import AutomataLean.Basic
+import AutomataLean.PDAs
+import AutomataLean.DPDAs
