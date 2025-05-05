6332e2bc6a8210b5803c33b72c91eacc08548c43
NaveenMaurya749 <77260143+NaveenMaurya749@users.noreply.github.com>
2025-05-05 15:14:08 +0530

    Update AutomataLean.lean

--- AutomataLean.lean
+++ AutomataLean.lean
@@ -2,5 +2,4 @@
 -- Import modules here that should be built as part of the library.
 import AutomataLean.Basic
 import AutomataLean.PDAs
-import AutomataLean.PDA_testing
 import AutomataLean.Parsing
